c114c3ff322e26fd5ae07b71b544ea3e31f03571
rafayhanan <mrafayhanan@gmail.com>
2024-08-03 01:54:48 +0500

    added homepage view

--- StoreFront/newapp/views.py
+++ StoreFront/newapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+'''
+View is a request handler.
+'''
+
+def home(request):
+    #return HttpResponse("Home Page")
+    return render(request,'index.html')
+
+def say_Hi(request):
+    return HttpResponse('Hi')
